5f500d0051da466f20a5c20a8e95f912841df811
Dmitry <manyakindima@gmail.com>
2023-10-22 17:46:11 +0300

    fix tiny mistake:)

--- todo/views.py
+++ todo/views.py
@@ -69,7 +69,7 @@ def currenttodos(request):
 
 
 def viewtodo(request, todo_pk):
-    todo = get_object_or_404(Todo, pk=todo_pk, user=request,user)
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'GET':
         form = TodoForm(instance=todo)
         return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
