73f337f4a84362ff3363409a4c6adcdd067e2fcd
Dmitry <manyakindima@gmail.com>
2023-10-22 18:32:42 +0300

    add complete and delete button

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.utils import timezone
+
 from .forms import TodoForm
 from .models import Todo
 
@@ -80,3 +82,18 @@ def viewtodo(request, todo_pk):
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'todo/viewtodo.html', {'todo': todo, 'error': 'Bad info'})
+
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+
+def deletetodo(request, todo_pk):
+    if request.method == 'POST':
+        todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+        todo.delete()
+        return redirect('currenttodos')
